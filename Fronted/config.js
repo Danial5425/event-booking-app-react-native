ec65cf2f119dc8e0d94d18ae1e557150329cc8e9
Danial <danikami5425@gmail.com>
2025-06-07 21:25:02 +0530

    Update config.js

--- Fronted/config.js
+++ Fronted/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = "http://ip:5000";
-GOOGLE_MAPS_API_KEY = "your api key";
+GOOGLE_MAPS_API_KEY = "your google api key";
 
 export default {
   API_BASE_URL: process.env.API_BASE_URL || "http://ip:5000",
